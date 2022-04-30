45223ee1fe8d5cd29bd100d8e3b1838ed48f38dd
lysa-thorn <lysa.thorn@gmail.com>
2022-04-30 16:39:03 +0700

    Update app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,13 +67,9 @@
                         </li>
                         @endif
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('shoes.index') }}">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('shoes.index') }}">Home</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('categories.index')}}">Categories</a>
                         </li>
