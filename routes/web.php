935f9e86ede2d80e5ab25ebfee897fbf1bbcffca
lisasouy <lysasouy@gmail.com>
2022-04-30 15:52:14 +0700

    crude category

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('admin/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard')->middleware('is_admin');
+// Route::get('/home', [App\Http\Controllers\CategoryController::class, 'index'])->name('category');
+Route::resource('/categories', \App\Http\Controllers\CategoryController::class);
+Route::get('/destroy/{id}', [\App\Http\Controllers\CategoryController::class, 'destroyCategory'])->name('categories.destroy');
